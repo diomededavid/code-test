253d91ada0884ab5a65eca4b0534482701833aa1
David Diomede <diomededavid@gmail.com>
2017-05-04 21:55:47 -0700

    Fixing jumpy media queries

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,5 @@
 $(document).foundation();
+
+$(window).on('changed.zf.mediaquery', function(event, newSize, oldSize) {
+  // newSize is the name of the now-current breakpoint, oldSize is the previous breakpoint
+});
